0ea9278eb85b937de01b8c693086d5a8faff13a8
Sifat Hasan <sifat120@outlook.com>
2021-05-04 10:11:35 -0700

    Added a little animation underneath the nav buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,6 +31,8 @@ function Header() {
                     <span>SERIES</span>
                 </a>
             </NavMenu>
+
+            <UserImg src="http://2.bp.blogspot.com/-iPolK7Rg1Yw/TiCJz-rlrBI/AAAAAAAAAzc/C9zIt8a8bz4/s1600/cute%2Bbaby%2Bpanda%2B012.jpg" />
         </Nav>
     )
 }
@@ -51,11 +53,14 @@ const Logo = styled.img`
 
 const NavMenu = styled.div`
     display: flex;
-
+    flex: 1;
+    margin-left: 20px;
+    align-items: center;
     a {
         display: flex;
         align-items: center;
         padding: 0 12px;
+        cursor: pointer;
         img {
             height: 20px;
         }
@@ -63,6 +68,35 @@ const NavMenu = styled.div`
         span {
             font-size: 13px;
             letter-spacing: 1.42px;
+            position: relative;
+
+            &:after {
+                content: "";
+                height: 2px;
+                background: white;
+                position: absolute;
+                left: 0;
+                right: 0;
+                bottom: -6px;
+                opacity: 0;
+                transform-origin: left center;
+                transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+                transform: scaleX(0);
+            }
+        }
+
+        &:hover {
+            span:after {
+                transform: scaleX(1);
+                opacity: 1;
+            }
         }
     }
+`
+
+const UserImg = styled.img`
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+    cursor: pointer;
 `
